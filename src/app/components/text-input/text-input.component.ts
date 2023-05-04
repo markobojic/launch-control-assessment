e9cf5c9cb2af4c8464bd9e0afbd24d39bd63bb70
markobojic <bojic.marko021@gmail.com>
2023-05-04 20:19:39 +0200

    refactor code and simplify solution

--- src/app/components/text-input/text-input.component.ts
+++ src/app/components/text-input/text-input.component.ts
@@ -9,56 +9,9 @@ export class TextInputComponent {
   text = '';
   highlightedText = '';
 
-  private squareBracketRegex = /\[([^\]]+)\]/g;
-  private curlyBracketRegex = /\{([^\}]+)\}/g;
-
   onInput() {
-    this.highlightedText = this.highlight(this.text);
-  }
-
-  private highlight(text: string): string {
-    const matches = [];
-    let match;
-
-    while ((match = this.squareBracketRegex.exec(text))) {
-      matches.push({
-        text: match[1],
-        squareBracket: true,
-        curlyBracket: false,
-        startIndex: match.index,
-        endIndex: this.squareBracketRegex.lastIndex - 1,
-      });
-    }
-
-    while ((match = this.curlyBracketRegex.exec(text))) {
-      matches.push({
-        text: match[1],
-        squareBracket: false,
-        curlyBracket: true,
-        startIndex: match.index,
-        endIndex: this.curlyBracketRegex.lastIndex - 1,
-      });
-    }
-
-    const highlightedText = [];
-    let lastIndex = 0;
-
-    for (const match of matches) {
-      if (match.startIndex >= lastIndex) {
-        highlightedText.push(text.slice(lastIndex, match.startIndex));
-        highlightedText.push(`
-          <span class="${
-            match.squareBracket ? 'square-bracket' : 'curly-bracket'
-          }">
-            ${match.text}
-          </span>
-        `);
-        lastIndex = match.endIndex + 1;
-      }
-    }
-
-    highlightedText.push(text.slice(lastIndex));
-
-    return highlightedText.join('');
+    this.highlightedText = this.text
+      .replace(/\[([^\]]+)\]/g, '<span class="square-bracket">$1</span>')
+      .replace(/\{([^\}]+)\}/g, '<span class="curly-bracket">$1</span>');
   }
 }
